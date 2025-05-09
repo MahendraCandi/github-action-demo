6c2b0d2721c5886903a20ae503161d75f09a472e
mahendracandi <abdumaadjidcandi@gmail.com>
2025-05-09 10:16:32 +0700

    add maven failsafe plugin

--- src/test/java/com/mahendracandi/github_actions_demo/WelcomeControllerIT.java
+++ src/test/java/com/mahendracandi/github_actions_demo/WelcomeControllerIT.java
@@ -5,21 +5,14 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.assertj.MockMvcTester;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class WelcomeControllerTest {
+class WelcomeControllerIT {
 
 	@LocalServerPort
 	int port;
